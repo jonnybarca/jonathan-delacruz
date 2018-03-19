10498ea23e875ff30ff880740cb7e275f821cb81
Jonathan Alexander De la cruz <jonathan.delacruz@stu.jefferson.kyschools.us>
2018-03-19 08:08:14 -0400

    added a dictionary.

---
+++ jonathan_delacruz-assign02.py
@@ -0,0 +1,20 @@
+#jonathan dictionary 03/15/18
+
+basketball_terms = {
+    
+    "Airball": "A shot at the basket that misses everything and doesn't touch the rim, backboard or net",
+    
+    "Charge": "A penalty, which results in a turnover, occurs when an offensive player" 
+        "with the ball runs into a stationary defensive player and knocks him or her over",
+        
+    "Double Dribble": "A penalty, which results in a turnover, occurs when a player dribbles the ball with both hands"
+        "It also occurs when a player dribbles, stops dribbling, and then begins to dribble again",
+   
+    "Elbow": "The area of the court where the free throw line meets the side of the key or paint",
+    
+    "Fast Break": "An offensive action where a team attempts to advance the ball and"
+        "score as quickly as possible after a steal, blocked shot or rebound",
+        
+    "Lay-Up": "A shot taken close to the hoop, usually when a player is moving toward the basket.",          }
+    
+print (basketball_terms)
